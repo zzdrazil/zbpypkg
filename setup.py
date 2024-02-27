ede14b910e73d5f8a292cdb288c891a1a480f1ef
Zbynek Zdrazil <zzdrazil@atrimax.com>
2024-02-27 17:08:25 +0100

    Version value fix.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
    name='zbpypkg',
-   version='1.0.0',
+   version='1.0.1',
    description='A simple Python test package',
    author='Zbynek Zdrazil',
    author_email='zbynek.zdrazil@merck.com',
