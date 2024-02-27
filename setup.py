799c7e2db8d3770f43838ba0b873dbba595cf18c
Zbynek Zdrazil <zzdrazil@atrimax.com>
2024-02-27 16:37:16 +0100

    Updated the packagestructure.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
    name='zbpypkg',
@@ -7,5 +12,11 @@ setup(
    author='Zbynek Zdrazil',
    author_email='zbynek.zdrazil@merck.com',
    packages=['zbpypkg'],  #same as name
+   keywords = "test",
+   python_requires=">=3.6, <4",
    install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+   project_urls={  # Optional
+        "Bug Reports": "https://github.com/zzdrazil/zbpypkg/issues",
+        "Source": "https://github.com/zzdrazil/zbpypkg.git",
+    },
 )
